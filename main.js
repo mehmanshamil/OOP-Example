11ff28c717b2e02d2506c73957a69dd18e29975a
mehmanshamil <sd8377n17@code.edu.az>
2024-08-20 17:59:50 +0400

    OOP task complated

---
+++ main.js
@@ -0,0 +1,28 @@
+// OOP Task
+// Employe adinda bir class yaradirsiz ad position salary qebul eden
+//  constructor qurun
+// daha sonra details deye function qurun burda name position 
+// ve salary dinamik olaraq versin
+// daha sonra ele bir functionn qurun ki ona faiz olaraq necese
+//  reqem verildiyinde hemin 
+// iscinin maasina o qeder % artim olsun meselen 100 manat 
+// alirsa 50% gonderdikde 150m olsun
+// elave olaraq bir static function qurun ve iki iscinin 
+// maaslarini muqayise elesin hansi coxdusa 
+// onu qaytarsin
+
+// Manager , AdministrativeStaff  adinda classlar yaradin ve onlarinda 
+// constructorunda name salary position olsun ve detail deye func olsun orda
+// constructorda gelen  name salary position haqqinda melumati eks etdirsin
+
+
+// Company adinda class yaradin constructor olaraq name qebul etsin ve gelen name 
+// uygun olaraq butun iscileri ozunde toplayan array qurun  company classi daxilinde
+// butun isciler haqqinda melumat veren function olsun
+// Sirketin toplam verilen maasini hesablayan function olsun
+// en cox maas alan isci haqqinda melumat veren function olsun 
+
+
+
+
+
